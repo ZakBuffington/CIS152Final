06015bad68a9776dce5730157db8f46996db458a
BuffingtonZ <59776753+BuffingtonZ@users.noreply.github.com>
2023-05-03 12:03:57 -0500

    Added manifest file for extension and basic script

---
+++ script.js
@@ -0,0 +1,17 @@
+async function fetchData() {
+
+}
+
+function authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
+        .then(function() { console.log("Sign-in successful"); },
+              function(err) { console.error("Error signing in", err); });
+}
+
+function loadClient() {
+    gapi.client.setApiKey("YOUR_API_KEY");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+}
